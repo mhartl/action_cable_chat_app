cdadf300102d2caf583169cf52e82fb6b2a1b40c
Michael Hartl <michael@michaelhartl.com>
2016-07-19 13:31:31 -0700

    Add signup form

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 20160719191240) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get new" do
+    get new_user_url
+    assert_response :success
+  end
+
+  test "should reject invalid submission" do
+    assert_no_difference 'User.count' do
+      post users_url,
+           params: { user: { username: "", password: "",
+                             password_confirmation: "" } }
+    end
+    assert_response :success
+    assert_select 'div#error_explanation'
+    assert_template 'users/new'
+  end
+
+  test "should accept valid submission" do
+    assert_difference 'User.count', 1 do
+      post users_url,
+           params: { user: { username: "example", password: "password",
+                             password_confirmation: "password" } }
+    end
+    assert_response :redirect
+  end
+end
