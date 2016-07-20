94dea6532d326f4b658f274c15205a9d707b9cd9
Michael Hartl <michael@michaelhartl.com>
2016-07-20 12:37:52 -0700

    Add Lee to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(username: "mhartl", password: "foobar")
+User.create!(username: "lee", password: "asdfasdf")
