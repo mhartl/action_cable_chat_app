555cd0fe15cce440c0dbf65572ddc2254a60327f
Michael Hartl <michael@michaelhartl.com>
2016-09-05 18:50:45 -0700

    Add name regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :messages
+  NAME_REGEX = /\w+/
   validates :username, presence: true, uniqueness: { case_sensitive: false },
-                       format: { with: /\A\w+\z/i },
+                       format: { with: /\A#{NAME_REGEX}\z/i },
                        length: { maximum: 15 }
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
