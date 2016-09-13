4b53cf2cef0d3302edaf138a0390b5a64b4d79dd
Michael Hartl <michael@michaelhartl.com>
2016-09-12 23:43:10 -0700

    Change limit -> last

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
-  scope :for_display, -> { order(:created_at).limit(50) }
+  scope :for_display, -> { order(:created_at).last(50) }
 end
