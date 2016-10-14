2b696e152b28190b4c996b19189540a3a9382fbf
Michael Hartl <michael@michaelhartl.com>
2016-10-14 11:42:54 -0700

    Finish basic enhancements

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include SessionsHelper
+
+    identified_by :message_user
+
+    def connect
+      self.message_user = find_verified_user
+    end
+
+    private
+
+      def find_verified_user
+        if logged_in?
+          current_user
+        else
+          reject_unauthorized_connection
+        end
+      end
+
   end
 end
