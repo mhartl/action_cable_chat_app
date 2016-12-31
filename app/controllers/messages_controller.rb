1bbf0cbcbf1bb7357f93a5d4c301887625eee166
jeanpaulsio <sio.jeanpaul@gmail.com>
2016-12-31 14:00:16 -0800

    basic action cable functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,9 +8,10 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to messages_url
-    else
-      render 'index'
+      ActionCable.server.broadcast 'room_channel',
+                                   content:  message.content,
+                                   username: message.user.username
+      head :ok
     end
   end
 
