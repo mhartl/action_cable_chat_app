a5adb2f5063e584a1190884be661aee2158f4359
darwin christopher tantuco <darwinxciii@yahoo.com>
2017-02-20 16:40:01 +0800

    simple action cable implementation

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
