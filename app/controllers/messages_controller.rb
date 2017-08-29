5569919f064d974a29c501ac47be9a0ae41e97bd
Adam <adamosch@gmail.com>
2017-08-29 15:41:03 +0200

    Add mentions to msg  model and controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,9 @@ class MessagesController < ApplicationController
     if message.save
       ActionCable.server.broadcast 'room_channel',
                                    message: render_message(message)
+      message.mentions.each do |mention|
+        ActionCable.server.broadcast "room_channel_user_#{mention.id}",
+                                     mention: true
     end
   end
 
