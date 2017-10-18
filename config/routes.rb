c03bea50279752d1d0d3f6b10d7961e298c0c6bf
Kamil Zieliński <kamil.zielinski@netguru.co>
2017-10-18 23:22:05 +0200

    provided full action-cable chat with metions

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  mount ActionCable.server, at: '/cable'
 end
