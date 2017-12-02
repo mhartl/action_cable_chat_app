7a1196d986fc43b95f921a85e2474fd6f09bb417
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-02 15:27:41 -0600

    setup connection authorization

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include SessionsHelper
+
+    def connect
+      self.message_user = find_verified_user
+    end
+
+    private
+      def find_verified_user
+        if logged_in?
+          current_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
