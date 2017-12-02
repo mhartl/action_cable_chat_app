b704d2a72c3ed1ff03bc7100ec7b96024b494d98
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-02 15:50:19 -0600

    refactoring

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,18 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    include SessionsHelper
+    # include SessionsHelper
 
-    def connect
-      self.message_user = find_verified_user
-    end
+    # def connect
+    #   self.message_user = find_verified_user
+    # end
 
-    private
-      def find_verified_user
-        if logged_in?
-          current_user
-        else
-          reject_unauthorized_connection
-        end
-      end
+    # private
+    #   def find_verified_user
+    #     if logged_in?
+    #       current_user
+    #     else
+    #       reject_unauthorized_connection
+    #     end
+    #   end
   end
 end
