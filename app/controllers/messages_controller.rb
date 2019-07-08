ebdbe6c1e50d1660a2af95e58149880bb3888744
Aditya <aditya@Adityas-MacBook-Air-2.local>
2019-07-08 16:11:46 +0530

    ActionCable.server.broadcast 'stream_name', <data_hash> method added to Message Controller - create, i.e whenever a message is created the necessary data is streamed across the 'room_channel' stream

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,7 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
+      ActionCable.server.broadcast 'room_channel', {content: message.content, username: message.user.username}
       redirect_to messages_url
     else
       render 'index'
