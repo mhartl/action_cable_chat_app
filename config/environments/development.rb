478e26efdf2d695d697d7343c1e7b589dd5ccaaa
Tia Stutson <tmstutson@gmail.com>
2020-01-12 00:19:34 +0000

    scroll bottom method added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # Add Cloud9 origin for Action Cable requests.
+  config.action_cable.allowed_request_origins = [
+    'https://rails-tutorial-mhartl.c9users.io' ]
 end
