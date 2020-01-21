58f8662e8db8abbb21991b65caa4c78790741ba5
Tia Stutson <tmstutson@gmail.com>
2020-01-21 00:14:13 +0000

    digest password error

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :messages
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-
+  
   mount ActionCable.server, at: '/cable'
 end
