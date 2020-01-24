2f82dcee54957c273a54d0ed89ca5e74d5f67033
Mason <mason.a.traylor@gmail.com>
2020-01-24 00:12:45 -1000

    Updates to make devise work with app

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,4 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false },
                        format: { with: /\A#{NAME_REGEX}\z/i },
                        length: { maximum: 15 }
-  validates :password, presence: true, length: { minimum: 6 }
-  has_secure_password
 end
