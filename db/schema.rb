6d2f1685cbb210d37372f2bfc75e0f3aba7ac21f
Anya Zenkina <anya.zenkina@shopify.com>
2021-12-16 18:51:07 +0000

    add coffee script

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719235623) do
+ActiveRecord::Schema.define(version: 2016_07_19_235623) do
 
   create_table "messages", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
+    t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
