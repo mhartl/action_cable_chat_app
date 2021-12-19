67cd50ca0989de88bd8a14ea8449ef1bec711d40
Anya Zenkina <anya.zenkina@shopify.com>
2021-12-19 17:57:12 +0000

    add action cable

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  mount ActionCable.server, at: '/cable'
 end
